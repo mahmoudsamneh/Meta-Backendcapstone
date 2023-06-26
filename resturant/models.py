312b790ff8b02e4ec7b73d93be90ef5040dc3be4
mahmoudsamneh <mahmoud.samneh@yahoo.com>
2023-06-26 08:10:15 +0300

    setting up models and mysql database

--- resturant/models.py
+++ resturant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    no_of_guests = models.IntegerField(default= 2, null= False,
+                                       validators=[
+                                           MaxValueValidator(6),
+                                           MinValueValidator(1)
+                                                    ]
+                                       )
+    booking_date = models.DateField(default=datetime.now, null=False)
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places= 2)
+    inventory = models.IntegerField(default=5)    
