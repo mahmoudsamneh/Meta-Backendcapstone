b719463f847116441b0739644e8e348bc1ed9a1f
mahmoudsamneh <mahmoud.samneh@yahoo.com>
2023-06-27 12:35:47 +0300

    set up menu and table APIs

--- resturant/urls.py
+++ resturant/urls.py
@@ -2,8 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.index, name = 'index'),
-    
+    path('',views.index, name = 'index'),  
+    path('menu/',views.MenuItemView.as_view(), name  = 'menu'),
+    path('menu/<int:pk>,', views.SingleMenuItemView.as_view(), name = 'singlemenu'),
+    path('booking/', views.BookingViewSet.as_view('list'), name = 'booking'),
     
     
 ]
--- resturant/views.py
+++ resturant/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics , viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
-
+# The views
 
 def index(request):
     return render(request,'index.html',{})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+        
+        
